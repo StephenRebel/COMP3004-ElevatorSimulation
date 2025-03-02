d81d8a65e52e25cd5eb012f77de3b124896c8189
Stephen Rebel <75142837+StephenRebel@users.noreply.github.com>
2025-03-01 20:31:19 -0500

    add

--- Building.h
+++ Building.h
@@ -7,6 +7,7 @@
 #include "Elevator.h"
 #include "Floor.h"
 #include "ElevatorControlSystem.h"
+#include "SafetySystem.h"
 
 class Building {
 
@@ -18,11 +19,13 @@ class Building {
 
     private:
         int numFloors;
+        int numElevators;
         std::string owner;
 
         std::vector<Elevator*> elevators;
         std::vector<Floor*> floors;
         ElevatorControlSystem* ECS;
+        SafetySystem* safetySys;
 };
 
 #endif
